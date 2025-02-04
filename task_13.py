5a44d7699a85dfe4379d567d2717c99bddbb9585
Sheroz Rakhimov <SRMUFA07@GMAIL.COM>
2025-02-04 16:59:50 +0500

    23

--- task_13.py
+++ task_13.py
@@ -24,14 +24,16 @@
 
 # Сеть задана IP-адресом 172.16.168.0 и маской сети 255.255.248.0.
 # Сколько в этой сети IP-адресов, для которых количество единиц в двоичной записи IP-адреса не кратно 5?
-from ipaddress import *
+# from ipaddress import *
 
-net = ip_network('172.16.168.0/255.255.248.0', 0)
-result = 0
+# net = ip_network('172.16.168.0/255.255.248.0', 0)
+# result = 0
 
-for ip in net:
-    ip_bin = f'{ip:b}'
-    if ip_bin.count('1') % 5 != 0:
-        result += 1
+# for ip in net:
+#     ip_bin = f'{ip:b}'
+#     if ip_bin.count('1') % 5 != 0:
+#         result += 1
 
-print(result)
+# print(result)
+
+
